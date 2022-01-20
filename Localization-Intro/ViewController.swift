19cce1989025c0d94f674f044a5f19c4ffb4b4cc
David Svensson <davidsvson@gmail.com>
2022-01-20 14:29:27 +0100

    first commit

--- Localization-Intro/ViewController.swift
+++ Localization-Intro/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+     //   imageView.image = UIImage(named: NSLocalizedString("animal", comment: ""))
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        helloLabel.text = NSLocalizedString("thank_you", comment: "")
+    }
+    
 }
 
